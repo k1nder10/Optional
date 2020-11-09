66ce42f41ea43055d30e46009a5325dea13536b5
Illya <extr1mme92@gmail.com>
2020-11-09 22:07:12 +0200

    Add boilerplate of the project

---
+++ palkin/optional.hpp
@@ -0,0 +1,13 @@
+namespace palkin {
+
+template <typename T>
+class OptionalStorage {
+public:
+};
+
+template <typename T>
+class Optional {
+public:
+};
+
+}  // namespace palkin
---
+++ test/optional_test.cpp
@@ -0,0 +1,10 @@
+#include "optional.hpp"
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+  std::cout << "Running tests...\n";
+  std::cout << "Done\n";
+
+  return 0;
+}
